25bfc63a176748fd9fe10ecd40d2ee75a975e5d6
moustafa-hegazy1 <moustafa.hegazy1@vodafone.com>
2023-05-27 20:00:08 +0300

    add bottom tab

--- NewsApp/App.tsx
+++ NewsApp/App.tsx
@@ -1,17 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, useColorScheme } from "react-native";
+import { StyleSheet, useColorScheme, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NewsScreen } from "./Screens/NewsScreen";
 import { NewsDetailsScreen } from "./Screens/NewsDetailsScreen";
 import { NewsContextProvider } from "./store/News-context";
 import { GlobalStyles } from "./utls/Colors";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SettingsScreen } from "./Screens/SettingsScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const BottomTab = createBottomTabNavigator();
   const theme = useColorScheme();
   const themedColors =
     theme === "dark" ? GlobalStyles.darkColors : GlobalStyles.lightColors;
+
+  function MyTabs() {
+    return (
+      <BottomTab.Navigator
+        screenOptions={{
+          tabBarBackground: () => (
+            <View style={{ backgroundColor: themedColors.headerColor }} />
+          )
+        }}
+      >
+        <BottomTab.Screen
+          name="NewsScreen"
+          component={NewsScreen}
+          options={{
+            headerShown: false,
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            )
+          }}
+        />
+        <BottomTab.Screen
+          name="SettingsScreen"
+          component={SettingsScreen}
+          options={{
+            headerShown: false,
+            tabBarLabel: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="settings" color={color} size={size} />
+            )
+          }}
+        />
+      </BottomTab.Navigator>
+    );
+  }
 
   return (
     <>
@@ -20,8 +59,8 @@ export default function App() {
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen
-              name="NewsScreen"
-              component={NewsScreen}
+              name="MyTabs"
+              component={MyTabs}
               options={{
                 title: "Apple News",
                 headerStyle: {
